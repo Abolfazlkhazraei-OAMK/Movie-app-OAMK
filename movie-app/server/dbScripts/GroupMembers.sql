bd3875666910f914c1011071cd4ae604823ec330
Didarul Islam Auny <156349692+didarulislam106@users.noreply.github.com>
2024-11-26 10:56:31 +0000

    Update GroupMembers.sql

--- movie-app/server/dbScripts/GroupMembers.sql
+++ movie-app/server/dbScripts/GroupMembers.sql
@@ -4,13 +4,13 @@ AFTER INSERT ON Groups
 FOR EACH ROW
 EXECUTE FUNCTION add_owner_to_group_members();
 
-CREATE TYPE member_status_enum AS ENUM ('pending', 'accepted', 'rejected');
+CREATE TYPE status_enum AS ENUM ('pending', 'accepted', 'rejected');
 
 CREATE TABLE GroupMembers (
     group_member_id SERIAL PRIMARY KEY,
     group_id INT REFERENCES Groups(group_id) ON DELETE CASCADE,
     user_id INT REFERENCES Users(user_id) ON DELETE CASCADE,
     is_owner BOOLEAN DEFAULT FALSE,
-    status member_status_enum DEFAULT 'pending',
+    status status_enum DEFAULT 'pending',
     joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
