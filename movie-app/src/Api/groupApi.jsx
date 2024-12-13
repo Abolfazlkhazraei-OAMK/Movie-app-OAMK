7d9e6e7e05a9ee76cb2920b795e440bc6a7e3a5e
rasmus-kaikkonen <t3kara00@students.oamk.fi>
2024-12-13 12:25:40 +0200

    Update groupApi.jsx

--- movie-app/src/Api/groupApi.jsx
+++ movie-app/src/Api/groupApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for the backend API
-const API_URL = 'http://localhost:3001/api/group';
+const API_URL = 'http://localhost:3001/group';
 
 
 // Create a new group
